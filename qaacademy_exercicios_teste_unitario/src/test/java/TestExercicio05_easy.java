c86f1b11079cac7e19fb67f8f4746af7918e7ab8
FernandaFonseca07 <fernanda_fonseca0107@hotmail.com>
2023-08-27 23:13:30 -0300

    Teste unitário Exercicio05_easy

---
+++ qaacademy_exercicios_teste_unitario/src/test/java/TestExercicio05_easy.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import qaacademy_exercicios_teste_unitario.Exercicios_easy.Exercicio05_easy;
+
+public class TestExercicio05_easy {
+    static Exercicio05_easy exercicio05_easy;
+
+    @BeforeClass
+    public static void beforeClass() {
+        exercicio05_easy = new Exercicio05_easy();
+    }
+
+    @Test
+    public void testeSoma() {
+        Assert.assertEquals(6, exercicio05_easy.soma(2, 2, 2));
+    }
+
+    @Test
+    public void testeSubtracao() {
+        Assert.assertEquals(-2, exercicio05_easy.subtracao(2, 2, 2));
+    }
+
+    @Test
+    public void testeMultiplicacao() {
+        Assert.assertEquals(8, exercicio05_easy.multiplicacao(2, 2, 2));
+    }
+
+    @Test
+    public void testeMedia() {
+        int soma = exercicio05_easy.soma(2, 2, 2);
+        Assert.assertEquals(2, exercicio05_easy.media(soma));
+    }
+}
